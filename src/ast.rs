934bf893d010ca2369205f5dab48a81b11ebc267
Matilde Morrone <contact@morrone.dev>
2025-06-17 21:26:20 +0200

    Parse array spreads only inside arrays

--- src/ast.rs
+++ src/ast.rs
@@ -30,6 +30,13 @@ pub enum PairOrSpread<'input> {
     Spread(&'input str),
 }
 
+/// Represents a chained key like "foo.bar.baz"
+#[derive(Debug, Clone)]
+pub struct ChainedKey<'input> {
+    /// The segments of the key path
+    pub segments: Vec<&'input str>,
+}
+
 /// An entry can be of various types as defined in the spec
 #[derive(Debug, Clone)]
 pub enum Entry<'input> {
@@ -44,18 +51,18 @@ pub enum Entry<'input> {
     /// Nested object
     Object(Object<'input>),
     /// Array of entries
-    Array(Vec<Entry<'input>>),
+    Array(Vec<EntryOrSpread<'input>>),
     /// Null value
     Null,
     /// Reference to an input variable
     Input(&'input str),
-    /// Array spread operation
-    Spread(&'input str),
 }
 
-/// Represents a chained key like "foo.bar.baz"
+/// Either a key or an array spread operation
 #[derive(Debug, Clone)]
-pub struct ChainedKey<'input> {
-    /// The segments of the key path
-    pub segments: Vec<&'input str>,
+pub enum EntryOrSpread<'input> {
+    /// An entry can be of various types as defined in the spec
+    Entry(Entry<'input>),
+    /// Array spread operation
+    Spread(&'input str),
 }
